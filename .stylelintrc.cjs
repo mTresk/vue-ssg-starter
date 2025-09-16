7cfb6b36062ed74812d36797640c8f3d3ab3c7bc
Maxim Tresk <djtresk@gmail.com>
2025-09-16 18:57:40 +0500

    fix: add stylelint rule to allow invalid position declarations

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -27,5 +27,6 @@ module.exports = {
     'no-descending-specificity': null,
     'declaration-property-value-no-unknown': null,
     'unit-disallowed-list': ['px'],
+    'no-invalid-position-declaration': null,
   },
 }
