4c39c95149a515b862b2c5950181081eebdc9122
Maxim Tresk <djtresk@gmail.com>
2025-10-04 14:58:36 +0500

    feat: add agreement functionality to manage form submission

---
+++ src/utils/base/agreement.ts
@@ -0,0 +1,33 @@
+function agreement() {
+  const forms = document.querySelectorAll('[data-form]') as NodeListOf<HTMLFormElement>
+
+  if (!forms.length) {
+    return
+  }
+
+  forms.forEach((form) => {
+    const submitButton = form?.querySelector('[data-submit]') as HTMLButtonElement
+    const checkbox = form?.querySelector('[data-checkbox]') as HTMLInputElement
+
+    if (!submitButton || !checkbox) {
+      return
+    }
+
+    const updateSubmitButton = () => {
+      if (checkbox.checked) {
+        submitButton.removeAttribute('disabled')
+      }
+      else {
+        submitButton.setAttribute('disabled', 'true')
+      }
+    }
+
+    updateSubmitButton()
+
+    checkbox.addEventListener('change', updateSubmitButton)
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  agreement()
+})
