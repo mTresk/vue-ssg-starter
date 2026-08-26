13f7d13094c226acdb7551fb942788716cfece58
Maxim Tresk <djtresk@gmail.com>
2026-08-26 16:56:02 +0500

    feat: add BaseImage component

---
+++ src/components/Base/BaseImage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface IProps {
+  src: string
+  alt?: string
+  width: number | string
+  height: number | string
+  loading?: 'lazy' | 'eager'
+}
+
+withDefaults(defineProps<IProps>(), {
+  alt: '',
+  loading: 'lazy',
+})
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    :loading="loading"
+  >
+</template>
