7ff3b2273e386f0b4775de85251c36a5e683e9e3
Maxim Tresk <djtresk@gmail.com>
2026-08-26 18:01:27 +0500

    refactor: improve input mask initialization and handle empty element case

--- src/utils/forms/inputmask.ts
+++ src/utils/forms/inputmask.ts
@@ -1,17 +1,26 @@
-import Inputmask from 'inputmask'
+import InputmaskLib from 'inputmask'
+
+type InputmaskConstructor = typeof InputmaskLib
+
+const Inputmask = (
+  (InputmaskLib as unknown as { default?: InputmaskConstructor }).default
+  ?? InputmaskLib
+) as InputmaskConstructor
 
 export function initMask() {
   const elements = document.querySelectorAll<HTMLInputElement>('[data-mask]')
 
-  if (elements.length) {
-    elements.forEach((element) => {
-      const inputmaskInstance = new Inputmask('+7 (999) 999 99 99', {
-        showMaskOnHover: false,
-      })
-
-      inputmaskInstance.mask(element)
-    })
+  if (!elements.length) {
+    return
   }
+
+  elements.forEach((element) => {
+    const inputmaskInstance = new Inputmask('+7 (999) 999 99 99', {
+      showMaskOnHover: false,
+    })
+
+    inputmaskInstance.mask(element)
+  })
 }
 
 window.addEventListener('DOMContentLoaded', () => {
